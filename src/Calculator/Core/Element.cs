6e14fd53da13a7bda33d1c9ec27cfb9844fa0a7b
Stevo John <stevehuwjohn@gmail.com>
2024-08-29 14:03:06 +0100

    WIP

--- src/Calculator/Core/Element.cs
+++ src/Calculator/Core/Element.cs
@@ -7,4 +7,14 @@ public abstract class Element
     public abstract void Process(Stack<Element> stack);
         
     public virtual double Value => throw new ParseException($"Incorrect call to .Value on Element type {GetType().Name}.");
+
+    public static Element Create(char symbol)
+    {
+        return new Operator(symbol);
+    }
+
+    public static Element Create(double value)
+    {
+        return new Operand(value);
+    }
 }
--- src/Calculator/Core/Parser.cs
+++ src/Calculator/Core/Parser.cs
@@ -2,4 +2,99 @@ namespace Calculator.Core;
 
 public class Parser
 {
+    private readonly Queue<Element> _queue = new();
+
+    private readonly Stack<char> _stack = new();
+
+    private int _position;
+
+    private string _expression;
+    
+    public Queue<Element> Parse(string expression)
+    {
+        Initialise(expression);
+        
+        while (_position < _expression.Length)
+        {
+            if (char.IsWhiteSpace(_expression[_position]))
+            {
+                _position++;
+                
+                continue;
+            }
+
+            if (ProcessForNumbers())
+            {
+                continue;
+            }
+
+            if (ProcessForParentheses())
+            {
+                continue;
+            }
+        }
+
+        return _queue;
+    }
+
+    private void Initialise(string expression)
+    {
+        _queue.Clear();
+        
+        _stack.Clear();
+
+        _position = 0;
+
+        _expression = expression;
+    }
+
+    private bool ProcessForNumbers()
+    {
+        if (! char.IsDigit(_expression[_position]))
+        {
+            return false;
+        }
+        
+        var start = _position;
+                
+        while (_position < _expression.Length && char.IsDigit(_expression[_position]))
+        {
+            _position++;
+        }
+                
+        var numberStr = _expression.Substring(start, _position - start);
+                
+        _queue.Enqueue(Element.Create(int.Parse(numberStr)));
+                
+        return true;
+
+    }
+
+    private bool ProcessForParentheses()
+    {
+        switch (_expression[_position])
+        {
+            case '(':
+                _stack.Push(_expression[_position]);
+
+                return true;
+                
+            case ')':
+            {
+                while (_stack.Count > 0 && _stack.Peek() != '(')
+                {
+                    _queue.Enqueue(Element.Create(_stack.Pop()));
+                }
+
+                if (_stack.Peek() == '(')
+                {
+                    _stack.Pop();
+                }
+
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
