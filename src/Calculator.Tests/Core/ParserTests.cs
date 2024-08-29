bb9430a418742d91b625501d161ba3bdb7755856
Stevo John <stevehuwjohn@gmail.com>
2024-08-29 16:52:02 +0100

    WIP

--- src/Calculator.Tests/Core/ParserTests.cs
+++ src/Calculator.Tests/Core/ParserTests.cs
@@ -16,6 +16,7 @@ public class ParserTests
     [InlineData("2.5 * 2", "2.5 2 *")]
     [InlineData("1 << 1 + 2", "1 1 2 + <<")]
     [InlineData("1 >> 1 + 2", "1 1 2 + >>")]
+    [InlineData("2 + -1", "2 1 -- +")]
     public void ParsesExpressionsCorrectly(string expression, string expected)
     {
         var result = _parser.Parse(expression);
@@ -43,7 +44,9 @@ public class ParserTests
                     "Divide" => "/ ",
                     "Exponentiate" => "^ ",
                     "LeftShift" => "<< ",
+                    "Modulus" => "% ",
                     "Multiply" => "* ",
+                    "Negate" => "-- ",
                     "RightShift" => ">> ",
                     "Subtract" => "- ",
                     _ => throw new TestException($"Unrecognised operation {operation}.")
