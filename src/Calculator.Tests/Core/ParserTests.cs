fc49a9c5540b084deed31168b81fc4c45f1d603d
Stevo John <stevehuwjohn@gmail.com>
2024-08-29 14:16:37 +0100

    WIP

--- src/Calculator.Tests/Core/ParserTests.cs
+++ src/Calculator.Tests/Core/ParserTests.cs
@@ -1,6 +1,18 @@
+using Calculator.Core;
+using Xunit;
+
 namespace Calculator.Tests.Core;
 
 public class ParserTests
 {
-    
+    private readonly Parser _parser = new();
+
+    [Theory]
+    [InlineData("1 + 5 * (8 - 2)", "1 5 8 2 - * +")]
+    private void ParsesExpressionsCorrectly(string expression, string expected)
+    {
+        var result = _parser.Parse(expression);
+        
+        Assert.Equal(expected, string.Join(' ', result));
+    }
 }
