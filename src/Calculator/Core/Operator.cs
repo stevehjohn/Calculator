83ef7a4b4b628d2d96f4bcfe1791cf3591aba71e
Stevo John <stevehuwjohn@gmail.com>
2024-08-29 13:46:01 +0100

    WIP

---
+++ src/Calculator/Core/Operator.cs
@@ -0,0 +1,9 @@
+namespace Calculator.Core;
+
+public class Operator : Element
+{
+    public override void Process(Stack<Element> stack)
+    {
+        throw new NotImplementedException();
+    }
+}
