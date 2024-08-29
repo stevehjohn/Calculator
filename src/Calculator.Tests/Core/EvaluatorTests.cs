7bb6b654c15d718419a7298b58deed0d3a790550
Stevo John <stevehuwjohn@gmail.com>
2024-08-29 16:14:39 +0100

    Wip

--- src/Calculator.Tests/Core/EvaluatorTests.cs
+++ src/Calculator.Tests/Core/EvaluatorTests.cs
@@ -11,6 +11,8 @@ public class EvaluatorTests
     [InlineData("2 * 3", 6)]
     [InlineData("2.5 / .5", 5)]
     [InlineData("5 * -3", -15)]
+    [InlineData("4 ^ 2", 16)]
+    [InlineData("4 ^ -2", 0.0625)]
     public void ProducesExpectedResult(string expression, double expected)
     {
         var result = _evaluator.Evaluate(expression);
