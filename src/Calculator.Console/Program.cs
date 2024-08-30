2587b689caef3c845af79829b5738f4a606ff4d1
Stevo John <stevehuwjohn@gmail.com>
2024-08-30 16:56:43 +0100

    wip

---
+++ src/Calculator.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using Calculator.Core;
+using Calculator.Infrastructure;
+
+namespace Calculator.Console;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var evaluator = new Evaluator(new EvaluationLogger(new ConsoleOutputProvider()));
+
+        System.Console.WriteLine();
+        
+        evaluator.Evaluate(args[0]);
+
+        System.Console.WriteLine();
+    }
+}
