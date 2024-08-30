a6e14bbf931f09e9788effe584c2e1f390e93332
Stevo John <stevehuwjohn@gmail.com>
2024-08-30 10:17:21 +0100

    wip

--- src/Calculator/Core/Evaluator.cs
+++ src/Calculator/Core/Evaluator.cs
@@ -1,3 +1,5 @@
+using Calculator.Infrastructure;
+
 namespace Calculator.Core;
 
 public class Evaluator
@@ -5,6 +7,18 @@ public class Evaluator
     private readonly Parser _parser = new();
 
     private readonly Stack<Element> _stack = new();
+
+    private readonly EvaluationLogger _logger;
+
+    public List<string> Steps => _logger?.GetSteps();
+    
+    public Evaluator(bool log = false)
+    {
+        if (log)
+        {
+            _logger = new EvaluationLogger();
+        }
+    }
     
     public double Evaluate(string expression)
     {
@@ -12,9 +26,13 @@ public class Evaluator
         
         _stack.Clear();
         
+        _logger?.SetUp(expression, _stack);
+        
         foreach (var element in queue)
         {
             element.Process(_stack);
+            
+            _logger?.StepComplete();
         }
 
         var result = _stack.Pop().Value;
