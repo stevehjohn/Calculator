5324f0718946c237defa30ecd8dccb995fbbbd26
Stevo John <stevehuwjohn@gmail.com>
2024-08-30 10:24:52 +0100

    wip

--- src/Calculator.Tests/Infrastructure/EvaluationLoggerTests.cs
+++ src/Calculator.Tests/Infrastructure/EvaluationLoggerTests.cs
@@ -1,6 +1,24 @@
+using Calculator.Core;
+using Xunit;
+
 namespace Calculator.Tests.Infrastructure;
 
 public class EvaluationLoggerTests
 {
+    private readonly Evaluator _evaluator = new(true);
     
+    [Theory]
+    public void OutputsCorrectSteps(string expression, string expected)
+    {
+        var expectedSteps = expected.Split('|');
+
+        _evaluator.Evaluate(expression);
+
+        var steps = _evaluator.Steps;
+
+        for (var i = 0; i < expectedSteps.Length; i++)
+        {
+            Assert.Equal(expectedSteps[i], steps[i]);
+        }
+    }
 }
