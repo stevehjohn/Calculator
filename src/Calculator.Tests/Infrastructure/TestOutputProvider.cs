f51ce5405cb4ea9fbf0250ae9f585f20aaaec183
Stevo John <stevehuwjohn@gmail.com>
2024-09-02 11:09:10 +0100

    wip

--- src/Calculator.Tests/Infrastructure/TestOutputProvider.cs
+++ src/Calculator.Tests/Infrastructure/TestOutputProvider.cs
@@ -7,9 +7,16 @@ public class TestOutputProvider : IOutputProvider
     private readonly List<string> _output = [];
 
     public IReadOnlyList<string> Output => _output;
+
+    public TestOutputProvider()
+    {
+        Console.WriteLine();
+    }
     
     public void WriteLine(string line)
     {
+        Console.WriteLine(line);
+        
         _output.Add(line);
     }
 }
