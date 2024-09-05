934234283e4e86a4a6abf693354fa3345c438bee
Stevo John <stevehuwjohn@gmail.com>
2024-09-05 13:34:19 +0100

    Wip

--- src/Calculator/Core/Element.cs
+++ src/Calculator/Core/Element.cs
@@ -6,7 +6,7 @@ namespace Calculator.Core;
 public abstract class Element
 {
     public abstract void Process(Stack<Element> stack, EvaluationLogger logger = null);
-        
+    
     public virtual double Value => throw new ParseException($"Incorrect call to .Value on Element type {GetType().Name}.");
     
     public static Element Create(string expression)
--- src/Calculator/Interfaces/ICreatableElement.cs
+++ src/Calculator/Interfaces/ICreatableElement.cs
@@ -4,5 +4,6 @@ namespace Calculator.Interfaces;
 
 public interface ICreatableElement<out T> where T : Element
 {
+    // ReSharper disable once UnusedMemberInSuper.Global - Enforces implementations
     static abstract T CreateInstance(string expression);
 }
